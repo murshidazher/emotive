415a67b6acc6e36f2e7453b074b5181f2236d264
murshidazher <murshidazher@yahoo.com>
2019-08-16 07:23:40 +0530

    fix(login.js): Fix login as a class

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,34 +2,41 @@ import React from 'react';
 import './Login.css';
 
 
-const Login = ({ onRouteChange }) => {
-    return ( 
-        <div className="middle br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
-        <main className="pa4 black-80">
-            <div className="measure center">
-                <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                <legend className="f4 fw6 ph0 mh0">Sign In</legend>
-                    <div className="mt3">
-                        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
-                    </div>
-                    <div className="mv3">
-                        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                       
-                        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
-                    </div>
-                </fieldset>
-                <div className="">
-                <button type="submit" className="btn" onClick={() => onRouteChange('home')}><span className="btn__content">Sign in</span></button>
-                </div>
-                <div className="lh-copy mt3">
-                <span onClick={() => onRouteChange('signup')} className="f6 link dim black db">Sign up</span>
-                </div>
-            </div>
-        </main>     
-        </div>
-    );
+class Login extends React.Component {
 
+    constructor() {
+        
+    }
+
+    render() {
+        return ( 
+            <div className="middle br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
+            <main className="pa4 black-80">
+                <div className="measure center">
+                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                    <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+                        <div className="mt3">
+                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
+                        </div>
+                        <div className="mv3">
+                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                           
+                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
+                        </div>
+                    </fieldset>
+                    <div className="">
+                    <button type="submit" className="btn" onClick={() => this.props.onRouteChange('home')}><span className="btn__content">Sign in</span></button>
+                    </div>
+                    <div className="lh-copy mt3">
+                    <span onClick={() => this.props.onRouteChange('signup')} className="f6 link dim black db">Sign up</span>
+                    </div>
+                </div>
+            </main>     
+            </div>
+        );
+    }
+  
 }
 
 export default Login;
