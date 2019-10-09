95a1e0d22ce2b8e35a7a447b6c477f7d5a27cbd8
murshidazher <murshidazher@yahoo.com>
2019-10-09 10:54:34 +0530

    feat(redis): Add redis for session

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -28,9 +28,9 @@ class Login extends React.Component {
       })
     })
       .then(resp => resp.json())
-      .then(user => {
-        if (user.id) {
-          this.props.loadUser(user);
+      .then(data => {
+        if (data.userId) {
+          this.props.loadUser(data);
           this.props.onRouteChange("home");
         }
       });
